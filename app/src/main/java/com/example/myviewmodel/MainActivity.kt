361b7d2a4096581941848724d6b8e97c1afdf92b
Fachmi Dimas Ardhana <fachmi2007@gmail.com>
2021-09-12 22:28:21 +0700

    project baru

---
+++ app/src/main/java/com/example/myviewmodel/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.myviewmodel
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myviewmodel.databinding.ActivityMainBinding
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
+import java.text.DecimalFormat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: WeatherAdapter
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = WeatherAdapter()
+        adapter.notifyDataSetChanged()
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+        binding.btnCity.setOnClickListener {
+            val city = binding.editCity.text.toString()
+            if (city.isEmpty()) return@setOnClickListener
+            showLoading(true)
+            setWeather(city)
+        }
+    }
+
+    fun setWeather(cities: String) {
+        val listItems = ArrayList<WeatherItems>()
+        val apiKey = "74d1a831d3fd073d26604fa1d2fede96"
+        val url = "https://api.openweathermap.org/data/2.5/group?id=${cities}&units=metric&appid=${apiKey}"
+        val client = AsyncHttpClient()
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Array<Header>, responseBody: ByteArray) {
+                try {
+                    //parsing json
+                    val result = String(responseBody)
+                    val responseObject = JSONObject(result)
+                    val list = responseObject.getJSONArray("list")
+                    for (i in 0 until list.length()) {
+                        val weather = list.getJSONObject(i)
+                        val weatherItems = WeatherItems()
+                        weatherItems.id = weather.getInt("id")
+                        weatherItems.name = weather.getString("name")
+                        weatherItems.currentWeather = weather.getJSONArray("weather").getJSONObject(0).getString("main")
+                        weatherItems.description = weather.getJSONArray("weather").getJSONObject(0).getString("description")
+                        val tempInKelvin = weather.getJSONObject("main").getDouble("temp")
+                        val tempInCelsius = tempInKelvin - 273
+                        weatherItems.temperature = DecimalFormat("##.##").format(tempInCelsius)
+                        listItems.add(weatherItems)
+                    }
+                    //set data ke adapter
+                    adapter.setData(listItems)
+                    showLoading(false)
+                } catch (e: Exception) {
+                    Log.d("Exception", e.message.toString())
+                }
+            }
+            override fun onFailure(statusCode: Int, headers: Array<Header>, responseBody: ByteArray, error: Throwable) {
+                Log.d("onFailure", error.message.toString())
+            }
+        })
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+}
